eb3c9befbcdfbd6faa6338633122ebfb4480f8a1
Robertas Plynius <robertasplynius@yahoo.com>
2018-06-23 22:07:48 -0500

    server.js added

---
+++ server.js
@@ -0,0 +1,91 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+// Configure middleware
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/tribscraper");
+
+
+// ROUTES
+
+// A GET route for scraping the Chicago Tribune website
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.bbc.com/news").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("article h2").each(function(i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+  .then(function(dbArticle, error) {
+    return res.json(dbArticle);
+  })
+  .catch(function(err) {
+    return res.json(err);
+  });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+ 
+  db.Note.find({})
+  .then(function(dbArticle) {
+    res.json(dbArticle);
+  })
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+
+  db.Note.create(req.body)
+  .then(function(dbNote) {
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, {note: dbNote._id}, {new: true });
+  })
+  .then(function(dbArticle) {
+    res.json(dbArticle)
+  })
+  .catch(function(err) {
+    res.json(err)
+  })
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
